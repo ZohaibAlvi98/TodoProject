3d4148c01064a4dcf53963041c5a999d099d5bce
ZohaibAlvi98 <zohaib.alvi90@gmail.com>
2020-01-30 00:36:24 +0500

    completed TodoProject

---
+++ todo.js
@@ -0,0 +1,30 @@
+$("ul").on("click","li",function(){
+  $(this).toggleClass("completed")
+      
+});
+
+
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  })
+       event.stopPropagation();
+ })
+
+ $("input[type='text'").on("keypress",function(e){
+   if(e.which === 13)
+   {
+     var todoText = $(this).val()
+     $(this).val("")
+     $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+   }
+ })
+
+ $("#toggle-form").click(function(){
+
+   $("input[type='text'").fadeToggle();
+ })
+
+
+
+ 
